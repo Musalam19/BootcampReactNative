323623232d94f6afe992e6fd63d9acf0028290e6
Sabil <muhammadsabililaslam@gmail.com>
2021-01-13 12:34:04 +0000

    string.js perubahan

--- string.js
+++ string.js
@@ -5,4 +5,6 @@ var fourth = 'and';
 var fifth = 'I'; 
 var sixth = 'love'; 
 var seventh = 'it!';
+var spasi = ' ';
 
+console.log(word.concat(second).concat(spasi).concat(third).concat(spasi).concat(fourth).concat(spasi).concat(fifth).concat(spasi).concat(sixth).concat(spasi).concat(seventh));
